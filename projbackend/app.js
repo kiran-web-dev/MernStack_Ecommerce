cff5f4abfd7efedc076f1246c392dbc13fe7d837
kiran-web-dev <kiranhegde1010@gmail.com>
2020-10-20 00:20:21 +0530

    initial setup, all models, routes and controllers for user and auth

---
+++ projbackend/app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//My routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+//DB connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+//My Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+
+//PORT
+const port = process.env.PORT || 8000;
+
+//Starting server
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
