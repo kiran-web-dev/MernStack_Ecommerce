27f8f47c4bb31aee5eedb0e55e7c5e83e3a6fb7a
kiran-web-dev <kiranhegde1010@gmail.com>
2020-10-23 01:43:46 +0530

    Completion of product controllers and routes

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -1,6 +1,11 @@
 const formidable = require("formidable");
 const Product =require("../models/product");
+const _= require("lodash");
 const fs =require("fs");
+const { sortBy } = require("lodash");
+const product = require("../models/product");
+//const { check, validationResult } = require("express-validator");
+
 
 
 exports.getProductById = (req, res, next, id) =>{
@@ -19,13 +24,14 @@ exports.getProductById = (req, res, next, id) =>{
 };
 
 exports.createProduct = (req, res) =>{
+    /*
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({
         error: errors.array()
       });
     }
-    
+    */
    const form = new formidable.IncomingForm();
    form.keepExtensions =true;
 
@@ -38,9 +44,8 @@ exports.createProduct = (req, res) =>{
        //destructure the fields
        const {name,price,description, category,stock} =fields;
 
-       //TODO:Recommended way to adding restrictions is in Routes itself (done)
+       //TODO:Recommended way to adding restrictions is in Routes itself 
 
-       /*
        //restriction for fields 
        if(
            !name || !price || !description || !category || !stock
@@ -49,7 +54,6 @@ exports.createProduct = (req, res) =>{
                error: "Please include all fields "
            })
        }
-       */
       
        let product = new Product(fields)
 
@@ -63,7 +67,7 @@ exports.createProduct = (req, res) =>{
            product.photo.data =fs.readFileSync(file.photo.path)
            product.photo.contentType =file.photo.type
        }
-       
+       //console.log(product);
        //save to DB
        product.save((err, product) =>{
            if(err){
@@ -77,4 +81,125 @@ exports.createProduct = (req, res) =>{
 
    });
 
+};
+
+exports.getProduct = (req, res) =>{
+    req.product.photo = undefined;
+    return res.json(req.product)
+};
+
+//Middleware for load photo optimization
+exports.photo = (req, res, next)=>{
+    if(req.product.photo.data){
+        res.set("Content-Type", req.product.photo.contentType)
+        return res.send(req.product.photo.data)
+    }
+};
+
+//Delete controllers
+exports.deleteProduct = (req, res) =>{
+    let product =req.product;
+    product.remove((err, deletedProduct)=>{
+        if(err){
+            return res.status(400).json({
+                error: "Failed to delete the product"
+            })
+        }
+        res.json({
+            message: "Deletion was success",
+            deleteProduct
+        })
+    })
+};
+
+//update contollers
+exports.updateProduct = (req, res) =>{
+    const form = new formidable.IncomingForm();
+    form.keepExtensions =true;
+ 
+    form.parse(req, (err, fields, file) =>{
+        if(err){
+            return res.status(400).json({
+                error: "Problem with image"
+            })
+        }
+       //Updation code
+        let product = req.product;
+        product = _.extend(product, fields)
+ 
+        //Handle file here
+        if(file.photo){
+            if(file.photo.size > 3000000){
+                return res.status(400).json({
+                    error: "file size too BIG!!!!"
+                })
+            }
+            product.photo.data =fs.readFileSync(file.photo.path)
+            product.photo.contentType =file.photo.type
+        }
+        //console.log(product);
+        //save to DB
+        product.save((err, product) =>{
+            if(err){
+                //return
+                res.status(400).json({
+                    error: "Updation of product Failed"
+                })
+            }
+            res.json(product);
+        })
+ 
+    });
+};
+
+//Listing 
+exports.getAllProducts = (req, res) =>{
+    let limit = req.query.limit ? parseInt(req.query.limit) : 8
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id"
+    Product.find()
+    .select("-photo")
+    .populate("category")
+    .sort([[sortBy, "asc"]])
+    .limit(limit)
+    .exec((err, products) =>{
+        if(err){
+            return res.status(400).json({
+                error: "No Product Found"
+            })
+        }
+        res.json(products)
+    })
+};
+
+exports.getAllUniqueCategories = (req, res) =>{
+    Product.distinct("category",{},(err, category)=>{
+        if(err){
+            return res.status(400).json({
+                error:"No Category Found"
+            })
+        }
+        res.json(category)
+    })
+};
+
+//BulkWrite for updating sold and stock
+exports.updateStock = (req, res, next) =>{
+    let myOperations = req.body.order.products.map(prod =>{
+        return {
+            updateOne: {
+                filter: {_id: prod._id},
+                update: {$inc: {stock: -prod.count, sold:+prod.count}}
+            }
+        }
+    })
+
+                        //operations,options, callback
+    Product.bulkWrite(myOperations,{}, (err, products)=>{
+        if(err){
+            return res.status(400).json({
+                error: "Bulk Operation Failed"
+            })
+        }
+        next();
+    })
 };
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,8 +1,17 @@
 const express =require("express")
 const router =express.Router()
-const { check, validationResult } = require("express-validator");
+//const { check, validationResult } = require("express-validator");
 
-const {getProductById, createProduct} =require("../controllers/product")
+const {
+    getProductById,
+    createProduct,
+    getProduct,
+    photo,
+    updateProduct,
+    deleteProduct,
+    getAllProducts,
+    getAllUniqueCategories
+} = require("../controllers/product")
 const { getUserById } =require("../controllers/user")
 const {isSignedIn,isAuthenticated,isAdmin} =require("../controllers/auth");
 const { options } = require("./auth");
@@ -16,6 +25,7 @@ router.param("productId",getProductById);
 //create
 router.post(
     "/product/create/:userId",
+    /*
     [
         check("name", "name should be atleast 3 char").isLength({ min: 3 }),
         check("price", "Price Shuould be number").isNumeric(),
@@ -23,12 +33,25 @@ router.post(
         check("stock", "stock should be number").isNumeric(),
         //check("category", "category should be atleast 5 char").isLength({ min: 5 }),
     ],
+    */
     isSignedIn,isAuthenticated,isAdmin,
     createProduct
 );
 
+//read routes
+router.get("/product/:productId", getProduct);
+router.get("/product/photo/:productId", photo);
+
 //update
+router.put("/product/:productId/:userId", isSignedIn,isAuthenticated,isAdmin, updateProduct);
 
 //remove
+router.delete("/product/:productId/:userId", isSignedIn,isAuthenticated,isAdmin, deleteProduct);
+
+//listing route
+router.get("/products", getAllProducts);
+
+router.get("/products/categories", getAllUniqueCategories);
+
 
 module.exports = router;
